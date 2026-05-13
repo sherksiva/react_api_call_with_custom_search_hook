e15000f8e9bfa05e4a5f359885f8fbca0e4d6cee
sherksiva <prakashraman441@gmail.com>
2026-05-13 19:46:28 +0530

    TO Do App

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@
  * Author : Sivaprakash Raman
  * Version : 0.0.1
  * Common component : Custom Search
- * Release : 0.0.1
- * Ticket : 1
+ * Release : 0.0.2
+ * Ticket : 1, 2
 */
 
 import { useState, useEffect } from 'react'
@@ -15,8 +15,12 @@ function App() {
   const [originalData, setOringinalData] = useState([]);
   const [searchInput, setSearchInput] = useState("");
 
-  const { customFilter, customPush, customData} = CustomSearch();
+  const [toDoValue, setToDoValue] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const [tempid, setTempid] = useState(0);
 
+  const { customFilter, customPush, customData} = CustomSearch();
+  // let id = 0;
   // Get Api Funnction
   const getApiData = async()=> {
 
@@ -35,6 +39,55 @@ function App() {
     }
 
   }
+
+  //ToDo Input Changes 
+  const onChangeToDo = (inputValue) => {
+    setToDoValue(inputValue);
+  }
+
+  // ToDo submit
+  const submitFormData = (event) => {
+    // console.log(event, "Event")
+    setTempid(tempid + 1);
+    // event.preventDefault();
+    let data = [{text: toDoValue, tempid, status: false}];
+    console.log(toDos, "Data")
+    setToDos([...toDos, ...data], ()=>{
+      console.log(toDos, "ToDos")
+    });
+    setToDoValue("");
+    event.preventDefault();
+  }
+
+  const deleteList = (tempIds) => {
+    let filteredData = toDos.filter((data) => data.tempid != tempIds);
+    console.log(filteredData, "Filtered Data");
+    setToDos(filteredData);
+  }
+
+  const editList = (data, index) => {
+
+    let tempData = [...toDos];
+     if(tempData[index]) {
+      tempData[index].status = true;
+
+      setToDos(tempData);
+      alert("Welcome to Edit");
+     }
+    // let editData = toDos.filter((data) => data.tempid == tempIds);
+    // console.log(editData, "Edit Data");
+  };
+
+  const savelist = (data, index) => {
+
+    let tempData = [...toDos];  
+      if(tempData[index]) {
+        tempData[index].status = false;
+
+        setToDos(tempData);
+        alert("Welcome to Save");
+      }
+    };
 
   // Clear Action 
   const clearAction = () => {
@@ -58,6 +111,7 @@ function App() {
 
   return (
     <>
+    {/* Search Main Component */}
       <section id="center">
         <h5>Custom Search with Http Api Request</h5>
         <br/>
@@ -77,6 +131,29 @@ function App() {
           customData && customData.length == 0 && 
 
           <span>No Record Found</span>
+        }
+      </section>
+      {/* ToDo List  */}
+      <section id="center" className="secton_top">
+        <h5>To Do </h5>
+        <form onSubmit={(e)=>submitFormData(e)}>
+          <div>
+            <input type="text" value={toDoValue} onChange={(e)=>onChangeToDo(e.target.value)} />
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+        {
+          toDos && toDos.length == 0 &&
+          <span>No Records Added</span>
+        }
+        {
+          toDos && toDos.length > 0 &&
+          <ul>
+            {toDos.map((data, index)=> 
+            <li key={index}>
+              <div>{ !data.status ? <span>{data.text}</span> : <input type="text" value={data.text} onChange={(e)=>{ let tempData = [...toDos]; tempData[index].text = e.target.value; setToDos(tempData); }} />} <span>{!data.status ? <button onClick={()=>editList(data,index)}>Edit</button> : <button onClick={()=>savelist(data,index)}>save</button>}<button onClick={()=>deleteList(data.tempid)}>Delete</button></span></div>
+              </li>)}
+          </ul>
         }
       </section>
     </>
